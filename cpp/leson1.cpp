19125ee6f3e1a6a55f0374068849e196f1117da2
Walti7239 <patwkeck@gmail.com>
2025-02-02 15:21:03 +0100

    alskdfj

---
+++ cpp/leson1.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <iterator>
+#include <string>
+
+using namespace std;
+
+enum class animal{
+  k9,
+  cat
+};
+
+class pet{
+  public:
+    std::string name;
+    int age;
+    animal kind; 
+  
+  std::string returnName(){
+    return name;
+  }
+  
+  int returnAge(){
+    return age;
+  }
+
+    void returnAnimalType(){
+    switch (kind) {
+      case animal::k9:
+        cout << "k9" << endl;
+      break;
+      case animal::cat:
+        cout << "cat" << endl;
+      break;
+    }
+  }
+  
+};
+
+int main(){
+ 
+  
+
+  pet mypet;
+  mypet.name = "test";
+  mypet.age = 23;
+  mypet.kind = animal::k9;
+  
+  std::cout << mypet.returnName() << std::endl;
+  std::cout << mypet.returnAge() << std::endl;
+  mypet.returnAnimalType();
+  return 0;
+}
