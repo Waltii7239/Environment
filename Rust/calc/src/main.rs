cbb7b41369bf26ef5d6de0fb500e9a38ddd249a3
Walti7239 <patwkeck@gmail.com>
2025-02-27 22:23:03 +0100

    asdf

--- Rust/calc/src/main.rs
+++ Rust/calc/src/main.rs
@@ -3,10 +3,15 @@
 use std::error::Error;
 slint::include_modules!();
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let ui: AppWindow = AppWindow::new().unwrap();
 
+
+fn main() -> Result<(), Box<dyn Error>> {
+    
+    let ui: AppWindow = AppWindow::new().unwrap();
     let ui_handle: slint::Weak<AppWindow> = ui.as_weak();
+    
+    let mut vec: Vec<i32> = Vec::new();
+
 
     ui.on_button_clicked(move  |button_id: i32| {
         if let Some(ui) = ui_handle.upgrade() {
@@ -15,26 +20,39 @@ fn main() -> Result<(), Box<dyn Error>> {
 
             match button_id {
                 
-                
-                2 => ui.set_display(current_value + 2),
-                3 => ui.set_display(current_value + 3),
-                4 => ui.set_display(current_value + 4),
-                5 => ui.set_display(current_value + 5),
-                6 => ui.set_display(current_value + 6),
-                7 => ui.set_display(current_value + 7),
-                8 => ui.set_display(current_value + 8),
-                9 => ui.set_display(current_value + 9),
-                0 => ui.set_display(current_value + 0),
-                _  => println!("well fuck now only god knows what this code dose"),
+                1 => { vec.push(1); }, 
+                2 => { vec.push(2); },  
+                3 => { vec.push(3); }, 
+                4 => { vec.push(4); }, 
+                5 => { vec.push(5); },  
+                6 => { vec.push(6); },
+                7 => { vec.push(7); }, 
+                8 => { vec.push(8); }, 
+                9 => { vec.push(9); }, 
+                0 => { vec.push(0); },
+               10 => {println!("Counter updated to: {:?}", vec_to_number(&mut vec));},  
+                _ => println!("well fuck now only god knows what this code dose"),
 
             }
-             
-            println!("Counter updated to: {:?}", current_value + 1);
         }
     });
 
     ui.run()?; 
-
     Ok(())
+}
+
+
+fn vec_to_number(vec: &mut Vec<i32>) -> i32{
+    
+    let size: i32 = vec.len() as i32; 
+    let mut number: i32 = 0;
+
+     
+    //for(int i = 0; i < size; i++)
+    for i in 0..size {
+        number = &number * 10 + vec[i as usize];  
+    }
+
+    return number;
 }
 
